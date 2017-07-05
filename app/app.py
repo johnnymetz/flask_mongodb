6b4ad0611eebbbb94bd6127f93cbae62c6703301
Jonathan Metz <jcmetz21@gmail.com>
2017-07-05 13:56:29 -0700

    Update method implemented. Flash macro added.

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, request, redirect, url_for, flash
+from flask import Flask, render_template, session, request, redirect, url_for, flash, Markup
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -8,8 +8,6 @@ app.config['SECRET_KEY'] = 'secret'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/myDB'
 
 mongo = PyMongo(app)
-
-
 
 
 @app.route('/', methods=['GET'])
@@ -22,20 +20,18 @@ def index():
     # Get data in current collection
     mongo_connection = mongo.db[active_collection]
     data = list(mongo_connection.find())
-    # print(data)
 
     return render_template('index.html', active_collection=active_collection, all_collections=all_collections,
                            data=data)
 
 
-@app.route('/add_data', methods=['POST'])
-def add_data():
+@app.route('/add', methods=['POST'])
+def add():
     """Add data to MongoDB"""
 
     # Build new document to be inserted
     new_document = {
         'name': request.form['name'],
-        'age': int(request.form['age']),
         'location': request.form['location']
     }
 
@@ -45,15 +41,15 @@ def add_data():
 
     # Feedback
     if insert.acknowledged:
-        flash('{} added to collection {}'.format(request.form['name'], request.form['collection']))
+        flash('{} added to {}'.format(request.form['name'], request.form['collection']), category='success')
     else:
-        flash('Something went wrong.')
+        flash('Something went wrong.', category='danger')
 
     return redirect(url_for('index'))
 
 
-@app.route('/delete_data/<id>', methods=['GET', 'POST'])
-def delete_data(id):
+@app.route('/delete/<id>', methods=['GET', 'POST'])
+def delete(id):
     """Delete data from MongoDB"""
 
     # Connect to MongoDB
@@ -64,12 +60,36 @@ def delete_data(id):
 
     # Feedback
     if delete.acknowledged:
-        flash('{} deleted from collection {}'.format(id, session.get('collection_name', 'myCollection')))
+        flash('{} deleted from {}'.format(id, session.get('collection_name', 'myCollection')), category='success')
     else:
-        flash('Something went wrong.')
+        flash('Something went wrong.', category='danger')
 
     return redirect(url_for('index'))
 
+
+@app.route('/update/<id>', methods=['POST'])
+def update(id):
+    """Update data in MongoDB"""
+
+    # Connect to MongoDB
+    mongo_connection = mongo.db[session.get('collection_name', 'myCollection')]
+
+    # Build updated document
+    updated_document = {
+        'name': request.form['name'],
+        'location': request.form['location']
+    }
+
+    # Update data by document unique ObjectID
+    update = mongo_connection.update_one({'_id': ObjectId(id)}, {'$set': updated_document})
+
+    # Feedback
+    if update.acknowledged:
+        flash('{} updated in {}'.format(id, session.get('collection_name', 'myCollection')), category='success')
+    else:
+        flash('Something went wrong.', category='danger')
+
+    return redirect(url_for('index'))
 
 
 @app.route('/change_collection', methods=['POST'])
@@ -80,13 +100,10 @@ def change_collection():
     session['collection_name'] = request.form['collection_name']
 
     # Feedback
-    flash('MongoDB collection changed to {}'.format(session['collection_name']))
+    flash(Markup('MongoDB collection changed to <strong>{}</strong>'.format(session['collection_name'])), category='success')
 
     return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
